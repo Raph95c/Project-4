3b9f81bf2cb9d57bb54a0248da7f5286d0e3929a
Nicholas Dao <nickdao86@gmail.com>
2023-03-31 21:43:29 -0700

    asks user for a song and gets track features

--- spotipy/spotipyxx.py
+++ spotipy/spotipyxx.py
@@ -30,6 +30,14 @@ def add_track(playlist):
 
     sp.playlist_add_items(playlist_id=playlist)
 
+def get_song_id(song_name):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
+    track_name, artist = song_name.split(" by ")
+    track_search = sp.search(q=f"Artist:{artist} track: {track_name}", type="track", limit=3)
+    track_id = track_search['tracks']['items'][0]['id']
+
+    return track_id;
+
 #right now this gets name, artist, and album for list of songs
 def get_song_info(song_id_list):
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
@@ -40,6 +48,27 @@ def get_song_info(song_id_list):
         song_artist = track["artists"][0]["name"]
         song_album = track["album"]["name"]
         print(f"{song_name} by {song_artist}")
+
+
+def get_track_features(track_id):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
+    feature_search = sp.audio_features(tracks=track_id)
+    acousticness = feature_search[0]['acousticness']
+    danceability = feature_search[0]['danceability']
+    duration_ms = feature_search[0]['duration_ms']
+    energy = feature_search[0]['energy']
+    instrumentalness = feature_search[0]['instrumentalness']
+    liveness = feature_search[0]['liveness']
+    loudness = feature_search[0]['loudness']
+    speechiness = feature_search[0]['speechiness']
+    tempo = feature_search[0]['tempo']
+    valence = feature_search[0]['valence']
+    features = {"acousticness":acousticness, "danceability":danceability, "duration_ms":duration_ms
+                ,"energy":energy, "instrumentalness":instrumentalness, "liveness":liveness, "loudness":loudness
+                , "speechiness":speechiness, "tempo":tempo, "valence":valence}
+    
+    return features;
+
 
 #prints out users top 10 tracks
 def user_top_tracks():
@@ -55,13 +84,6 @@ def user_top_tracks():
             print(i, item['name'], '//', item['artists'][0]['name'])
         print()
 
-
-#song list from track clustering
-song_list = ['7MdZQXUBijQxcBkYz4G1Z5',
- '0zhzkkSXFj60Y5fzb2j9hU',
- '7v52k3ozNqjKGlFPWDiudw',
- '6ZGqVXaJmhbYAK2DF8mWRR',
- '3V0nnQhqvbE3JmiDdnzQFQ']
 
 def main():
     #setting environment variables for spotipy
@@ -79,8 +101,14 @@ def main():
     response = requests.post(url, data=data, headers=headers)
     response_json = response.json()
     access_token = response_json['access_token']
-    get_song_info(song_list)
+
+    song_name = input("What song do u like? (song by artist)")
+    track_id = get_song_id(song_name)
+    track_features = get_track_features(track_id)
+    print(track_features)
+
 
 
 if __name__ == "__main__":
     main()
+
